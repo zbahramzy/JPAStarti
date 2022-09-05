91123b51d9b254292ba9eaaccb98b6298be47ab0
zbahramzy <zalandbahramzy@gmail.com>
2022-09-05 14:18:07 +0200

    fixed unit test

--- src/test/java/com/example/jpastarti/JpaStartiApplicationTests.java
+++ src/test/java/com/example/jpastarti/JpaStartiApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.jpastarti.model.Student;
 import com.example.jpastarti.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest
+@DataJpaTest
 class JpaStartiApplicationTests {
 
     @Autowired
@@ -23,15 +24,19 @@ class JpaStartiApplicationTests {
 
     @Test
     void testStudent() {
-        /*
         Student std1 = new Student();
         std1.setBorn(LocalDate.now());
         std1.setName("Kaj");
         studentRepository.save(std1);
 
+        Student std2 = new Student();
+        std1.setBorn(LocalDate.now());
+        std1.setName("Hauv");
+        studentRepository.save(std2);
+
         List<Student> list = studentRepository.findAll();
         //assertEquals(3, list.size());
-        assertEquals(true, list.size()>10);
+        assertEquals(true, list.size()<10);
 
         studentRepository.delete(std1);
         list = studentRepository.findAll();
@@ -42,8 +47,6 @@ class JpaStartiApplicationTests {
             Student std3 = opt.get();
             assertEquals(std3.getName(), "Kaj");
         }
-         */
-
     }
 
 }
